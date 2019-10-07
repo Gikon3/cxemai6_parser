dfb5afa8fd24b3666b223b57b24e95f4fe10bef4
aliguk <gukov0aleksey@gmail.com>
2019-10-07 15:24:20 +0300

    add flag_delete, fix count, change filename output, add check opcode, small fix

--- delete_bad_package.py
+++ delete_bad_package.py
@@ -3,18 +3,30 @@ import json
 
 from defines import *
 
-filename_in_list = ["logs_in/session_409/summary_409_correct.log",
-                    "logs_in/session_410/U_28.09.2019_23-41-46_correct.log",
-                    "logs_in/session_411/U_29.09.2019_00-21-12_correct.log"]
+filename_in_list = ["logs_in/session_409/temp/summary_409_alignment.log",
+                    "logs_in/session_410/temp/U_28.09.2019_23-41-46_alignment.log",
+                    "logs_in/session_411/temp/U_29.09.2019_00-21-12_alignment.log"]
+filename_addresses = "addresses.txt"
+
+with open(filename_addresses, 'r') as file_addr:
+    blocks_addresses = json.load(file_addr)
+
+addresses = [addr for addr in blocks_addresses]
 
 for filename_in in filename_in_list:
+    print("Calculate", filename_in)
+
     filename_delete_pack = "{0:s}/time_deleted_packages.log".format(os.path.split(filename_in)[0])
 
     with open(filename_in, 'r') as file_in:
         lines_in = file_in.readlines()
 
+    print(len(lines_in), end=" -> ")
+
+    rave = []
     del_time = []
     count = 0
+    number_errors = None
     while True:
         try:
             if lines_in[count][27:35] == COM_FLAG_ERROR:
@@ -32,7 +44,11 @@ for filename_in in filename_in_list:
                 number_errors = number_errors if (number_errors * 2) < (THRESHOLD_ERRORS * 2) else THRESHOLD_ERRORS
                 count += 1
 
+                flag_delete = False
                 for errors_count in range(number_errors):
+                    if lines_in[count][27:35] not in addresses:
+                        flag_delete = True
+
                     if lines_in[count][27:35] == COM_ALRM_TMR \
                             or lines_in[count][27:35] == COM_UNRESET_DEVICE:
                         count += 1
@@ -47,18 +63,40 @@ for filename_in in filename_in_list:
                         del_time.append([time_last, lines_in[count][:26] if lines_in[count][:26] != "_" * 26 \
                             else lines_in[count - 1][:26]])
                         del lines_in[count_last:count]
+                        count = count_last - 1
                         break
 
                     elif lines_in[count + 1][27:35] == COM_FLAG_ERROR:
                         del_time.append([time_last, lines_in[count + 1][:26] if lines_in[count + 1][:26] != "_" * 26 \
                             else lines_in[count][:26]])
                         del lines_in[count_last:count + 1]
-                        count -= count - count_last + 1
-                        print(lines_in[count][27:35])
+                        count = count_last - 1
                         break
 
                     count += 2
 
+                if flag_delete is True:
+                    del_time.append([time_last, lines_in[count + 1][:26] if lines_in[count + 1][:26] != "_" * 26 \
+                        else lines_in[count][:26]])
+                    del lines_in[count_last:count - 1]
+                    count = count_last - 1
+
+                # count += 1
+            elif lines_in[count][27:35] == COM_OK:
+                count += 2
+
+            elif lines_in[count][27:35] == COM_UNRESET_DEVICE \
+                or lines_in[count][27:35] == COM_ALRM_TMR \
+                or lines_in[count][27:35] == COM_err_CORR_IRQ \
+                or lines_in[count][27:35] == COM_TIMEOUT_SPI \
+                or lines_in[count][27:35] == COM_STM_START:
+                    count += 1
+
+            elif lines_in[count][27:35] == COM_MEM_IRQ_start:
+                count += 3
+
+            else:
+                rave.append(lines_in[count][:-1])
                 count += 1
 
                 # if lines_in[count + 1][27:35] != COM_FLAG_ERROR\
@@ -92,16 +130,16 @@ for filename_in in filename_in_list:
             # elif lines_in[count][27:35] == COM_RAVE:
             #     count += 3
 
-            else:
-                # print(count + 1, lines_in[count][27:35])
-                # exit()
-                count += 1
-
         except IndexError:
             break
+
+    print("{0:d}, deleted {1:d} packets".format(len(lines_in), len(del_time)))
 
     with open(filename_in, 'w') as file_out:
         file_out.writelines(lines_in)
 
     with open(filename_delete_pack, 'w') as file_del_pack:
         json.dump(del_time, file_del_pack, indent=2)
+
+    with open("rave/rave_{0:s}".format(os.path.split(filename_in)[1]), 'w') as file_rave:
+        json.dump(rave, file_rave, indent=2)
