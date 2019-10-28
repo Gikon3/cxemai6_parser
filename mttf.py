baced98912634daa0d4fcb9a47b8f7575c35e6a7
aliguk <gukov0aleksey@gmail.com>
2019-10-28 15:30:30 +0300

    uncomment average

--- mttf.py
+++ mttf.py
@@ -29,18 +29,18 @@ tmp_list = []
 for filename_irq_mem, filename_unreset in zip(filename_interval_irq_mem_list, filename_interval_unreset_list):
     with open(filename_irq_mem, 'r') as file_irq_mem:
         line_list = [line[:-1] for line in file_irq_mem.readlines()]
-        # average_irq_mem = float(line_list[-1][10:])
-        tmp_list = [float(num) for num in line_list[:-1]]
-        average_irq_mem = max(tmp_list)
+        average_irq_mem = float(line_list[-1][10:])
+        # tmp_list = [float(num) for num in line_list[:-1]]
+        # average_irq_mem = max(tmp_list)
 
     with open(filename_unreset, 'r') as file_unreset:
         line_list = [line[:-1] for line in file_unreset.readlines()]
-        # average_unreset = float(line_list[-1][10:])
-        tmp_list = [float(num) for num in line_list[:-1]]
-        average_unreset = max(tmp_list)
+        average_unreset = float(line_list[-1][10:])
+        # tmp_list = [float(num) for num in line_list[:-1]]
+        # average_unreset = max(tmp_list)
 
-    # T_list.append((average_irq_mem + average_unreset) / 2)
-    T_list.append(max(average_irq_mem, average_unreset))
+    T_list.append((average_irq_mem + average_unreset) / 2)
+    # T_list.append(max(average_irq_mem, average_unreset))
 
 session = 143
 for sigma, T, time in zip(sigma_list, T_list, time_list):
